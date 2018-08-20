4be7b0a3e1f8144ce063cc98fb640b67133f38fc
Yunding Li <yli@c02wj13vhv2f.corp.robot.car>
2018-08-20 16:57:02 -0700

    implement operation

---
+++ Converter.cpp
@@ -0,0 +1,45 @@
+#include "Converter.h"
+//
+//ExtractAddress::ExtractAddress(std::string address) {
+//    folderPath = new std::filesystem::path(address);
+//    DirectoryIterator = new std::filesystem::directory_iterator(*folderPath);
+//}
+
+
+void ConverterOperation::storeCSVData(std::string address,std::map<FrameId, OrignalData> &storeFiles){
+    std::fstream file(address);
+    OrignalData fileData;
+    std::string value;
+    std::vector<std::string> temporaryStore;
+    int firstLine = 1;
+    while(file.good()){
+        if(firstLine){
+            std::getline(file, value);
+            firstLine--;
+        }else{
+            int counter = 1;
+            while(std::getline(file, value)){
+                std::stringstream ss(value);
+                std::string subvalue;
+                while (std::getline(ss, subvalue, ',')) {
+                    counter++;
+                    if(counter == 18){
+                        std::cout<<"wait"<<std::endl;
+                    }
+                    temporaryStore.push_back(value);
+                    std::cout<<subvalue<<std::endl;
+                }
+                
+            }
+
+            }
+//            if(fileData.ID == std::stoi(temporaryStore[0])){
+//
+//            }
+            
+        }
+    }
+
+
+
+
